a3c9706790ba42e58fda6f67f3881e4704513b6f
Grayson Earle <graysonearle@gmail.com>
2017-12-04 23:11:00 -0500

    imported another example

---
+++ Assets/Hover Text/hover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour {
+
+	public string HoverText;
+	public TextMesh DisplayTextObject;
+
+	void OnMouseEnter() {
+		// switch the 3d text so that its text value
+		// is now whatever text value this hover instance holds
+		DisplayTextObject.text = HoverText;
+
+		// show it
+		DisplayTextObject.color = Color.white;
+	}
+
+	void OnMouseExit() {
+		DisplayTextObject.color = new Color(0,0,0,0);
+	}
+}
